fa914e204ad51d75a57ab2bf4d5be0d18634fac3
dilusha sangeeth <dilushasanga@gmail.com>
2018-11-20 20:47:25 +0800

    angular project created

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-app - this is my first angular app with MacBook Pro 2018';
+}
